5980a8e32ca38fe612ce24bf241d781ab2b8bd81
Wei Jie Xu <62730551+xuweijie82@users.noreply.github.com>
2024-09-07 01:28:05 +0800

    First commit
    Parsing tool that scans for dead code and deprecate

---
+++ parsing-tool/src/main/java/com/lib/util/engine/tool/constant/EngineToolConstant.java
@@ -0,0 +1,16 @@
+package com.lib.util.engine.tool.constant;
+
+public class EngineToolConstant {
+
+  public static final String DEPRECATE = "Deprecated";
+  public static final String DEPRECATED = "@" + DEPRECATE + "()";
+  public static final String NEW_LINE = "\n";
+  public static final String AT = "@";
+  public static final String SPACE = " ";
+  public static final String JAVA_EXTENSION = ".java";
+  public static final String COMMENT = "Deprecated by XWJ Backend Tool";
+  public static final String GET = "get";
+  public static final String SET = "set";
+  public static final String IS = "is";
+  public static final String INIT = "init";
+}
